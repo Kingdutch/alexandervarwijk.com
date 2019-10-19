af295695e86157adfc6372ea737f05fbed81ee18
Alexander Varwijk <git@alexandervarwijk.com>
2019-10-19 21:44:28 +0200

    Fix footer spacing

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,12 +35,21 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
+      </div>
+      <footer>
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          © {new Date().getFullYear()} by Alexander Varwijk, proudly built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+        </div>
+      </footer>
     </>
   );
 };
