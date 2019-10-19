0d1e1c9eaf18380a9b06f2c7608234461759b168
Alexander Varwijk <git@alexandervarwijk.com>
2019-10-19 15:41:32 +0200

    Set site meta-data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Alexander's Blog`,
+    description: `The thoughts and musings of Alexander Varwijk - Full Stack developer.`,
+    author: `@Kingdutch`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
