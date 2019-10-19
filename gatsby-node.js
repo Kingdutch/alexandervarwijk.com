077b2f88d2aa32317a0060fa843eb85976f3d203
Alexander Varwijk <git@alexandervarwijk.com>
2019-10-19 17:30:28 +0200

    Configure prettier
    It's just personal preference but I like comma's and single-quotes where
    possible.

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,10 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-const { createFilePath } = require("gatsby-source-filesystem")
+const { createFilePath } = require('gatsby-source-filesystem');
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
 
   if (node.internal.type === `MarkdownRemark`) {
     // Add the collection to our node so it can be used in querying.
@@ -22,11 +22,11 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     const slugPrefix = {
       blog: '/blog/',
       pages: '',
-    }
+    };
     createNodeField({
       name: `slug`,
       node,
-      value: `${slugPrefix[collection]}${createFilePath({ node, getNode})}`,
-    })
+      value: `${slugPrefix[collection]}${createFilePath({ node, getNode })}`,
+    });
   }
-}
+};
