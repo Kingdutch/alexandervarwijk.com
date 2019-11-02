0614903ee7e29667a81abea787f94afb3529acc3
Alexander Varwijk <git@alexandervarwijk.com>
2019-11-02 16:46:05 +0100

    Fix avatar sizing on mobile

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -5,8 +5,21 @@
  */
 
 import React from 'react';
+import styled from 'styled-components';
 import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
+
+const StyledImg = styled(Img)`
+  border-radius: 100%;
+  width: 60px;
+  height: 60px;
+  flex: auto 0 0;
+  
+  @media (min-width: 560px) {
+    width: 75px;
+    height: 75px;
+  }
+`;
 
 function Avatar({ style }) {
   const { file } = useStaticQuery(
@@ -14,8 +27,8 @@ function Avatar({ style }) {
       query {
         file(relativePath: { eq: "alexandervarwijk.jpeg" }) {
           childImageSharp {
-            fixed(width: 75, height: 75) {
-              ...GatsbyImageSharpFixed
+            fluid(maxWidth: 75, maxHeight: 75) {
+              ...GatsbyImageSharpFluid
             }
           }
         }
@@ -24,13 +37,10 @@ function Avatar({ style }) {
   );
 
   return (
-    <Img
-      fixed={file.childImageSharp.fixed}
+    <StyledImg
+      fluid={file.childImageSharp.fluid}
       alt={'A headshot of Alexander Varwijk'}
-      style={{
-        borderRadius: '100%',
-        ...style,
-      }}
+      style={style}
     />
   );
 }
