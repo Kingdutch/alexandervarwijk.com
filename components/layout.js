a0e77e9e59aff6f36de3ee1ba7d9c04332cec949
Alexander Varwijk <git@alexandervarwijk.com>
2020-11-24 23:53:12 +0100

    Replace skip-content target element
    This satisfies lighthouse because it's on longer an invalid link. The
    behaviour is the same.

--- components/layout.js
+++ components/layout.js
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
         <a href="#content" className="text-blue-600 sr-only focus:not-sr-only">Skip to main content</a>
       </div>
       <Header siteTitle={"Alexander Varwijk"} />
-      <a id="content" />
+      <span id="content" />
       {children}
       <footer className="mx-auto px-4 py-6 max-w-2xl lg:max-w-3xl lg:px-0">
         <div>
