dcdea4e65525a17c3e069bda0fd7e353036a1c95
Alexander Varwijk <git@alexandervarwijk.com>
2020-11-29 21:50:37 +0100

    Migrate tracking code to Next.js
    Brief testing shows this should still work :) We still need to move away
    from the `unload` handler to improve page performance but that is not
    something we'll do now as we don't want to break a working thing.

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,23 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
 
 class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+        </Head>
         <body>
           <Main />
           <NextScript />
+          <script
+            key="main"
+            async
+            src={'//visit.alexandervarwijk.com/hello.js'}
+          ></script>,
+          <noscript>
+            <img src={'//visit.alexandervarwijk.com/image.gif'} alt={''} />
+          </noscript>
         </body>
       </Html>
     )
