5f24e5dd6464271bb3ff15eb72d3de54fa4b589f
Alexander Varwijk <git@alexandervarwijk.com>
2021-01-03 16:25:50 +0100

    Fix chart responsiveness issue
    See https://github.com/plouc/nivo/issues/1290

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ const withMDX = require('@next/mdx')({
 })
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx'],
+  webpack: config => {
+    config.module.rules.push({
+      test: /react-spring/,
+      sideEffects: true,
+    })
+
+    return config
+  },
 })
